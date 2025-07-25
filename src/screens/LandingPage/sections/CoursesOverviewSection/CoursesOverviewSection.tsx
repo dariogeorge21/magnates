c89583c509d5ae5b511bdb83ccf45caae064f2b0
Dario George <edu.dariogeorge21@gmail.com>
2025-07-25 19:00:55 +0530

    auto scroll

--- src/screens/LandingPage/sections/CoursesOverviewSection/CoursesOverviewSection.tsx
+++ src/screens/LandingPage/sections/CoursesOverviewSection/CoursesOverviewSection.tsx
@@ -72,7 +72,7 @@ export const CoursesOverviewSection = (): JSX.Element => {
               scrollItems.map((item, index) => (
                 <div key={`scrollitem-${dupIdx}-${index}-${item}`} className="flex items-center">
                   <img
-                    src="/public/Overview.svg"
+                    src="https://res.cloudinary.com/dobqxxtml/image/upload/v1753445584/Overview_kv9coo.svg"
                     alt="Certification icon"
                     className="w-[37px] h-[37px]"
                     style={{ minWidth: 37, minHeight: 37 }}
